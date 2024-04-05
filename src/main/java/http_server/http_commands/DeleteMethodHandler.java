9a4b54852558fa57aacc88c4c561caa31dfbc0ad
oleksii.syzonov <oleksii.syzonov@ericsson.com>
2024-04-05 14:47:00 +0300

    start cicd 4

--- src/main/java/http_server/http_commands/DeleteMethodHandler.java
+++ src/main/java/http_server/http_commands/DeleteMethodHandler.java
@@ -21,7 +21,7 @@ public class DeleteMethodHandler extends HttpMethodHandler{
             }
             else {
                 executeSendResponseHeaders(ex, StatusCode._500.getCode(), response.getBytes().length);
-            }//cicd check Attempt 3
+            }//cicd check Attempt 4
         }
     }
 }
